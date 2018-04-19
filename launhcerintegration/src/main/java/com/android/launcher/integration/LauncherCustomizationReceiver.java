4bd6a71368f8025517967ed3ef2c3fbcfd0fbd2b
Vo7ice <Vo7ice@outlook.com>
2018-04-19 15:36:48 +0800

    新增gms包中launcher的扩展

---
+++ launhcerintegration/src/main/java/com/android/launcher/integration/LauncherCustomizationReceiver.java
@@ -0,0 +1,20 @@
+package com.android.launcher.integration;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author huguojin
+ */
+public class LauncherCustomizationReceiver extends BroadcastReceiver {
+
+    public LauncherCustomizationReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    }
+
+
+}
