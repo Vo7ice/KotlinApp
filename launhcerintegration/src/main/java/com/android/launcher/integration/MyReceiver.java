02d857dfb2f6ed0781cb35f3072fb376193fab4e
Vo7ice <Vo7ice@outlook.com>
2018-06-26 20:11:29 +0800

    PTT

--- launhcerintegration/src/main/java/com/android/launcher/integration/MyReceiver.java
+++ launhcerintegration/src/main/java/com/android/launcher/integration/MyReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.view.KeyEvent;
 
 /**
  * @author huguojin
@@ -11,11 +12,17 @@ import android.util.Log;
 public class MyReceiver extends BroadcastReceiver {
 
     private static final String TAG = "MyReceiver";
+    private static final String PTT_INTENT = "com.kodiak.intent.action.PTT_Button";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "onReceive: action = " + intent.getAction());
         /*intent.setComponent(new ComponentName("com.android.launcher.integration",
                 "com.android.launcher.integration.MyReceiver"));*/
+        KeyEvent event = intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+        Intent pttIntent = new Intent(PTT_INTENT);
+        pttIntent.putExtra(Intent.EXTRA_KEY_EVENT, event);
+        pttIntent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        context.sendBroadcast(pttIntent);
     }
 }
