558793749061fc42d05e30f13ba997f06205db29
Vo7ice <Vo7ice@outlook.com>
2018-07-30 20:14:20 +0800

    timer 替换方案

--- launhcerintegration/src/main/java/com/android/launcher/integration/MainActivity.java
+++ launhcerintegration/src/main/java/com/android/launcher/integration/MainActivity.java
@@ -35,6 +35,10 @@ import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Timer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
     private static final int MSG_VALIDATE_NETWORK_DONE = 1;
     private static final int MSG_VALIDATE_SMS_DONE = 2;
     private static final int MSG_VALIDATE_CALL_DONE = 3;
+    private static final int MSG_NETWORK_AVAILABLE_NOW = 4;
+    private static final int MSG_TERMINATE_ALL = 5;
 
     private static final int MSG_RESULT_TRUE = 1;
     private static final int MSG_RESULT_NOT_PERFORM = 0;
@@ -62,6 +68,12 @@ public class MainActivity extends AppCompatActivity {
     private static final String TABLE_NAME = "favorites";
 
     public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + TABLE_NAME);
+
+    Timer mTimer;
+
+    private ScheduledExecutorService mExecutorService;
+    private RepeatTask mRpeatTask;
+    private TerminateTask mTerminateTask;
 
 
     @Override
@@ -82,9 +94,12 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.test_call);
         SpannableString span = new SpannableString(getResources().getString(R.string.app_name));
-        span.setSpan(new URLSpan("tel:18885460314"),0, span.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        span.setSpan(new URLSpan("tel:18885460314"), 0, span.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         textView.setText(span);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        mExecutorService = Executors.newScheduledThreadPool(2);
+
 
     }
 
@@ -95,6 +110,13 @@ public class MainActivity extends AppCompatActivity {
         mSmsAync = new SmsAync(MainActivity.this);
         mCallAync = new CallAync(MainActivity.this);
         mH = new H(MainActivity.this);
+        mRpeatTask = new RepeatTask(mH, getBaseContext());
+        mTerminateTask = new TerminateTask(mH);
+        if (!mExecutorService.isShutdown()) {
+            mExecutorService.scheduleAtFixedRate(mRpeatTask, 1, 150, TimeUnit.MICROSECONDS);
+            mExecutorService.scheduleAtFixedRate(mTerminateTask, 2000, 1, TimeUnit.MICROSECONDS);
+        }
+
 
         TelephonyManager tm = (TelephonyManager) MainActivity.this.getSystemService(Context.TELEPHONY_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
@@ -120,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
         doSomthing();
 
         String selection = "itemType" + " = ? " + " AND " + " title = ?";
-        String[] selectionArgs = new String[] {"0", "Voicemail"};
+        String[] selectionArgs = new String[]{"0", "Voicemail"};
         ContentResolver resolver = getBaseContext().getContentResolver();
         Cursor cursor = resolver.query(CONTENT_URI, null, selection, selectionArgs, "_id DESC");
         if (null == cursor) {
@@ -130,11 +152,11 @@ public class MainActivity extends AppCompatActivity {
         }
         long id = 0L;
         if (null != cursor && cursor.moveToFirst()) {
-             id = cursor.getLong(cursor.getColumnIndex("_id"));
+            id = cursor.getLong(cursor.getColumnIndex("_id"));
         }
         if (id != 0L) {
             String where = "_id = ?";
-            String[] ids = new String[] {String.valueOf(id)};
+            String[] ids = new String[]{String.valueOf(id)};
             ContentValues values = new ContentValues();
             values.put("cellX", 3);
             values.put("cellY", 3);
@@ -143,6 +165,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Resources resources = getBaseContext().getResources();
+
 
     }
 
@@ -161,20 +184,33 @@ public class MainActivity extends AppCompatActivity {
         if (!mNetworkAync.isCancelled()) {
             mNetworkAync.cancel(true);
         }
+        if (!mExecutorService.isTerminated() || !mExecutorService.isShutdown()) {
+            mExecutorService.shutdownNow();
+        }
     }
 
     protected void handleReceiveMessage(Message msg) {
         int what = msg.what;
+        Log.d("Vo7ice", "receive message" + what);
         switch (what) {
             case MSG_VALIDATE_NETWORK_DONE:
                 msg.what = MSG_VALIDATE_SMS_DONE;
                 msg.arg1 = MSG_RESUTL_FALSE;
-                mH.sendMessageDelayed(msg,1000);
-
+                mH.sendMessageDelayed(msg, 1000);
                 break;
             case MSG_VALIDATE_SMS_DONE:
                 break;
             case MSG_VALIDATE_CALL_DONE:
+                break;
+            case MSG_NETWORK_AVAILABLE_NOW:
+            case MSG_TERMINATE_ALL:
+                Log.d("Vo7ice", " !mExecutorService.isShutdown() = " + !mExecutorService.isShutdown());
+                Log.d("Vo7ice", "isTerminated = " + mExecutorService.isTerminated());
+                if (mExecutorService != null && !mExecutorService.isShutdown()) {
+                    mExecutorService.shutdownNow();
+                }
+                break;
+            default:
                 break;
         }
     }
@@ -188,6 +224,7 @@ public class MainActivity extends AppCompatActivity {
     private class SmsDeliverReceiver extends BroadcastReceiver {
 
         private H mHandler;
+
         public SmsDeliverReceiver(H handler) {
             mHandler = handler;
         }
@@ -210,8 +247,46 @@ public class MainActivity extends AppCompatActivity {
                     msg.arg1 = MSG_RESUTL_FALSE;
                     mHandler.sendMessageDelayed(msg, NETWORK_GAP);
                     break;
+                default:
+                    break;
             }
             // unRegisterReceiver();
+        }
+    }
+
+    public static class RepeatTask implements Runnable {
+
+        private H mH;
+        private Context mContext;
+        private int count;
+
+        public RepeatTask(H h, Context context) {
+            mH = h;
+            mContext = context;
+        }
+
+        @Override
+        public void run() {
+            count++;
+            Log.d("Vo7ice", "count = " + count + ", isDataAvailable(mContext) = " + isDataAvailable(mContext));
+            if (isDataAvailable(mContext)) {
+                mH.sendEmptyMessage(MSG_NETWORK_AVAILABLE_NOW);
+            }
+        }
+    }
+
+    public static class TerminateTask implements Runnable {
+
+        private H mH;
+
+        public TerminateTask(H h) {
+            mH = h;
+        }
+
+        @Override
+        public void run() {
+            Log.d("Vo7ice", "Terminate");
+            mH.sendEmptyMessage(MSG_TERMINATE_ALL);
         }
     }
 
@@ -324,19 +399,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    protected boolean isCellularConnect(Context context) {
+    protected static boolean isCellularConnect(Context context) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo ni = cm != null ? cm.getActiveNetworkInfo() : null;
         return null != ni && ni.getType() == ConnectivityManager.TYPE_MOBILE;
     }
 
-    protected boolean isWifiConnect(Context context) {
+    protected static boolean isWifiConnect(Context context) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo ni = cm != null ? cm.getActiveNetworkInfo() : null;
         return null != ni && ni.getType() == ConnectivityManager.TYPE_WIFI;
     }
 
-    protected boolean isDataAvailable(Context context) {
+    protected static boolean isDataAvailable(Context context) {
         return isWifiConnect(context) || isCellularConnect(context);
     }
 
